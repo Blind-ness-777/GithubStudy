4138cbd572c0b16d543a66cf43c894560ad430b6
Blind-ness-777 <wlsgod10@gmail.com>
2025-12-17 10:52:57 +0900

    게임 초기 필드 데이터 선언

--- 251217/ConsoleProject/ConsoleProject/Program.cs
+++ 251217/ConsoleProject/ConsoleProject/Program.cs
@@ -18,11 +18,70 @@
 
 class Program
 {
-    // 플레이어, 폭탄, 목표지점, 벽, 빈공간
+    private const char PLAYER = 'P';            // 플레이어
+    private const char PLAYER_ON_GOAL = '@';    // 플레이어가 목표지점 위에 있는 상태
+    private const char BOMB = 'B';              // 폭탄
+    private const char BOMB_ON_GOAL = '!';      // 폭탄이 목표지점 위에 있는 상태
+    private const char GOAL = 'G';              // 목표지점
+    private const char WALL = '#';              // 벽
+    private const char EMPTY = ' ';             // 빈공간
+
+    private static char[,] map = new char[,] // 게임 필드(문자 기반 2차원 배열)
+    {
+        { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' },
+        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+        { '#', ' ', ' ', 'B', ' ', ' ', ' ', ' ', ' ', '#' },
+        { '#', ' ', ' ', ' ', 'P', 'G', ' ', ' ', ' ', '#' },
+        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+        { '#', ' ', ' ', 'B', ' ', ' ', 'G', ' ', ' ', '#' },
+        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+        { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' }
+    };
+    
+    static Position _playerPos = new Position()
+    {
+        X = 4,
+        Y = 4
+    };
+    
+    static int _moveCount = 0;
     
     static void Main(string[] args)
     {
-        
-        
+        // 안내 멘트 출력
+        PrintMap();
+
+        while (true)
+        {
+            // 게임 로직
+        }
     }
+
+    static void PrintGuideText()
+    {
+        Console.Clear();
+        Console.WriteLine("W : 위로 / S : 아래로 / A : 왼쪽 / D : 오른쪽 / Q : 종료");
+        Console.WriteLine("모든 폭탄을 목표지점으로 옮기세요");
+        Console.WriteLine();
+    }
+
+    static void PrintMap()
+    {
+        for (int i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                Console.WriteLine(map[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+public struct Position()
+{
+    public int X;
+    public int Y;
 }
