cfca5bd47cc60dd97b5a45a7f4a8b4041f03f4cb
Blind-ness-777 <wlsgod10@gmail.com>
2025-12-27 04:57:54 +0900

    Update Program.cs

--- 251227/ConsoleApp1/ConsoleApp1/Program.cs
+++ 251227/ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,13 +2,14 @@
 
 class Program
 {
-    static int Main(string[] args)
+    static void Main(string[] args)
     {
         Console.Clear();
         
+        /*
         int answer = 0;
         int n = Convert.ToInt32(Console.ReadLine());
-
+        
         if (n % 2 == 0)
         {
             for (int i = n; i > 0; i -= 2)
@@ -25,5 +26,17 @@ class Program
         }
         
         return answer;
+        */
+    }
+    
+    public string[] solution(string[] strArr)
+    {
+        for (int i = 0; i < strArr.Length; i++)
+        {
+            if (i % 2 == 0) strArr[i] = strArr[i].ToLower();
+            else strArr[i] = strArr[i].ToUpper();
+        }
+        
+        return strArr;
     }
 }
