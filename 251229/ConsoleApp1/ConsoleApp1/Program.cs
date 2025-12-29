1d9b65746acd61262d342527ee8b2ffeee045972
Blind-ness-777 <wlsgod10@gmail.com>
2025-12-29 12:31:46 +0900

    Update Program.cs

--- 251229/ConsoleApp1/ConsoleApp1/Program.cs
+++ 251229/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace ConsoleApp1;
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace ConsoleApp1;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Console.Clear();
         
+        
+    }
+
+    static void PrintList(LinkedList<int> list)
+    {
+        foreach (int l in list)
+        {
+            Console.Write($"[{l}]");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+public class Monster
+{
+    public string Name { get; private set; }
+
+    public Monster(string name)
+    {
+        Name = name;
     }
 }
