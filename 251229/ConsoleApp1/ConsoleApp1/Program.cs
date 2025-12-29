d02cc57179852548bd0d186edd084732cc429259
Blind-ness-777 <wlsgod10@gmail.com>
2025-12-29 18:11:54 +0900

    실습 과제 1번

--- 251229/ConsoleApp1/ConsoleApp1/Program.cs
+++ 251229/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ConsoleApp1;
 
@@ -9,33 +10,67 @@ class Program
     {
         Console.Clear();
 
-        Queue<Unit> barracks = new();
-        
-        barracks.Enqueue(new Unit("Marine"));
-        barracks.Enqueue(new Unit("Ghost"));
-        barracks.Enqueue(new Unit("Firebat"));
-        barracks.Enqueue(new Unit("Medic"));
-        barracks.Enqueue(new Unit("Marine"));
+        VendingMachine machine = new VendingMachine();
 
-        Console.WriteLine(barracks.Count);
-        
-        Unit unit = barracks.Dequeue();
+        int value = 0;
 
-        Console.WriteLine("--------------------");
-
-        foreach (Unit u in barracks)
+        while (true)
         {
-            Console.WriteLine(u.Name);
+            if (!int.TryParse(Console.ReadLine(), out value))
+            {
+                Console.WriteLine("숫자를 입력해주세요.");
+                continue;
+            }
+            
+            if (value <= 0)
+            {
+                Console.WriteLine("1 이상의 숫자를 입력해주세요.");
+                continue;
+            }
+
+            break;
+        }
+        
+        for (int i = 0; i < value; i++)
+        {
+            machine.AddMilk(new Milk());
         }
     }
 }
 
-public class Unit
+public class Milk
 {
-    public string Name { get; private set; }
-
-    public Unit(string name)
+    public int ExpirationDate { get; private set; }
+    
+    public Milk()
     {
-        Name = name;
+        ExpirationDate = 7;
+    }
+}
+
+public class VendingMachine
+{
+    Queue<Milk> milkQueue = new Queue<Milk>();
+
+    public void AddMilk(Milk milk)
+    {
+        milkQueue.Enqueue(milk);
+        Console.WriteLine("우유가 보충되었습니다.");
+        Console.WriteLine($"남은 우유 : {milkQueue.Count}");
+    }
+
+    public void RemoveMilk()
+    {
+        if (milkQueue.Count <= 0)
+        {
+            Console.WriteLine("자판기에 우유가 없습니다.");
+            return;
+        }
+        
+        Milk milk = milkQueue.Dequeue();
+        
+        Console.WriteLine($"꺼낸 우유의 유통기한 : {milk.ExpirationDate}");
+        
+        Console.WriteLine($"자판기에 우유가 {milkQueue.Count}개 남아 있습니다.");
     }
 }
