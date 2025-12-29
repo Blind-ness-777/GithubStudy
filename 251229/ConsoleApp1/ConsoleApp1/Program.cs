22ddaa21b9caabbca5a5ca452fe9e1d022ae2e3b
Blind-ness-777 <wlsgod10@gmail.com>
2025-12-29 14:11:30 +0900

    List<T>

--- 251229/ConsoleApp1/ConsoleApp1/Program.cs
+++ 251229/ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,21 +9,53 @@ class Program
     {
         Console.Clear();
         
+        List<Monster> list = new List<Monster>();
+
+        list.Add(new Monster("오크"));
+        PrintList(list);
+
+        Monster slime1 = new("슬라임");
+        list.Add(slime1);
+        PrintList(list);
         
+        list.Add(new Monster("해골"));
+        PrintList(list);
+        
+        list.Insert(1, new Monster("아이루"));
+        PrintList(list);
+
+        Console.WriteLine(list[2].Name);
+
+        Monster slime2 = new("슬라임");
+        if (list.Contains(slime1))
+        {
+            Console.WriteLine("슬라임 찾았음");
+        }
+
+        list.Remove(slime1);
+        PrintList(list);
+        
+        list.RemoveAt(0);
+        PrintList(list);
     }
 
-    static void PrintList(LinkedList<int> list)
+    static void PrintList(List<Monster> list)
     {
-        foreach (int l in list)
+        foreach (Monster l in list)
         {
-            Console.Write($"[{l}]");
+            Console.Write($"[{l.Name}]");
         }
 
         Console.WriteLine();
     }
 }
 
-public class Command
+public class Monster
 {
-    public int a;
+    public string Name { get; private set; }
+
+    public Monster(string name)
+    {
+        Name = name;
+    }
 }
