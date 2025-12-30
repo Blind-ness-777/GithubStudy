260d2d497e1db3dfd4fe18ce86cd378c6e9c80ae
Blind-ness-777 <wlsgod10@gmail.com>
2025-12-30 12:16:20 +0900

    그래프 구현

--- 251230/ConsoleApp1/ConsoleApp1/Program.cs
+++ 251230/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,46 +6,56 @@ class Program
     {
         Console.Clear();
 
-        Dictionary<string, int> dict = new();
+        MatrixGraph<string> graph = new(4);
         
-        dict.Add("이승열", 59);
-        dict.Add("이인", 19);
-
-        dict.TryAdd("이인", 59);
-        // TryAdd로 굳이 같은 키 값을 지닌 것을 추가하는 것 권장하지 않음
-
-        dict["최원탁"] = 12;
-
-        Console.WriteLine(dict["최원탁"]);
-
-        if (!dict.ContainsKey("송근형"))
-        {
-            dict.Add("송근형", 92);
-        }
-        else
-        {
-            // 추가할 수 없었을 때의 처리
-        }
-
-        // dict.Remove("이인");
-
-        Console.WriteLine(dict["이인"]);
-
-        foreach (KeyValuePair<string, int> item in dict)
-        {
-            Console.WriteLine($"key = {item.Key}, value = {item.Value}");
-        }
-
-        Console.WriteLine(dict.Count);
-        
-        
-        // 키 값을 int로 할 경우는 List가 더 어울린다. 단, 목적성이 어울리면 사용해도 되나 이 자료구조를 왜 사용했는지 설명하면 된다.
-        Dictionary<int, Item> monsters = new();
-        List<Item> list = new();
+        graph.AddEdgeCycle(0, 1, 1);
+        graph.AddEdgeCycle(0, 2, 1);
+        graph.AddEdgeCycle(1, 2, 1);
+        graph.AddEdgeCycle(1, 3, 1);
+        graph.AddEdgeCycle(2, 3, 1);
     }
 }
 
-public class Item
+public class ListGraph
 {
+    private List<int>[] _arr;
+
+    public void AddEdge(int from, int to)
+    {
+        AddUnique(_arr[from], to);
+    }
+
+    public void AddUnique(List<int> list, int value)
+    {
+        if (!list.Contains(value))
+        {
+            list.Add(value);
+        }
+    }
+}
+
+
+public class MatrixGraph<T>
+{
+    private int[,] _matrix;
+
+    public MatrixGraph(int size)
+    {
+        _matrix = new int[size, size];
+    }
     
+    public void AddEdge(int from, int to, int value)
+    {
+        if (value <= 0) return;
+        
+        _matrix[from, to] = value;
+    }
+    
+    private void AddEdgeCycle(int a, int b, int value)
+    {
+        if (!value <= 0)
+        {
+            
+        }
+    }
 }
