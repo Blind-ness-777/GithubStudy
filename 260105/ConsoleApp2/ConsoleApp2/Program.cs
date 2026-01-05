72f04564e15210915b474462077ce87e9cc0af42
Blind-ness-777 <wlsgod10@gmail.com>
2026-01-05 11:57:07 +0900

    Merge Sort 구현

--- 260105/ConsoleApp2/ConsoleApp2/Program.cs
+++ 260105/ConsoleApp2/ConsoleApp2/Program.cs
@@ -5,22 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.Clear();
-        
+
         List<int> list = new List<int>();
-        
-        list.Add(7);
-        list.Add(3);
-        list.Add(9);
-        list.Add(1);
+
         list.Add(5);
-        list.Add(8);
         list.Add(2);
-        
+        list.Add(8);
+        list.Add(3);
+        list.Add(1);
+        list.Add(6);
+        list.Add(4);
+        list.Add(7);
+
         PrintList(list);
         Console.WriteLine();
-        
-        QuickSort(list, 0, list.Count - 1);
-        
+
+        MergeSort(list, 0, list.Count - 1);
+
         PrintList(list);
     }
 
@@ -32,43 +33,66 @@ class Program
         }
     }
 
-    static void QuickSort(List<int> list, int left, int right)
+    static void MergeSort(List<int> list, int left, int right)
     {
         if (left >= right) return;
+        int center = (left + right) / 2;
 
-        int pivot = Partition(list, left, right);
-        
-        QuickSort(list, left, pivot - 1);
-        QuickSort(list, pivot + 1, right);
-        
+        MergeSort(list, left, center);
+        MergeSort(list, center + 1, right);
+
+        Merge(list, left, center, right);
     }
 
-    static int Partition(List<int> list, int left, int right)
+    static void Merge(List<int> list, int left, int center, int right)
     {
-        int pivot = list[right];
-        int i = left - 1;
-        
-        // 리스트 내부에서 pivot보다 작은 값들은 왼쪽으로.
-        for (int j = left; j < right; j++)
+        // 임시 배열의 크기 = 현재 병합하는 범위의 길이
+        int[] temp = new int[right - left + 1];
+
+        int leftIndex = left;
+        int rightIndex = center + 1;
+        int tempIndex = 0; // temp에서 사용할 인덱스
+
+        // 두 범위에서 남아있는 동안 계속 비교하고, 작은 값을 temp로 이동
+        while (leftIndex <= center && rightIndex <= right)
         {
-            if (list[j] < pivot)
-            {
-                i++;
-                Swap(list, i, j);
-            }
-        }
-        
-        // pivot을 작은 값 바로 다음 영역으로 이동시키고 고정.
-        int pivotIndex = i + 1;
-        Swap(list, pivotIndex, right);
-        
-        return pivotIndex;
-    }
+            // 불안정 정렬 [2a][1][2b][3] => [1][2b][2a][3]
+            // 안정 정렬 [2a][1][2b][3] => [1][2a][2b][3]
 
-    static void Swap(List<int> list, int a, int b)
-    {
-        int temp = list[a];
-        list[a] = list[b];
-        list[b] = temp;
+            if (list[leftIndex] <= list[rightIndex]) // 왼쪽을 우선시 함
+            {
+                temp[tempIndex] = list[leftIndex];
+                leftIndex++;
+            }
+            else
+            {
+                temp[tempIndex] = list[rightIndex];
+                rightIndex++;
+            }
+
+            tempIndex++;
+        }
+
+        // 왼쪽 범위에 남은 값이 있다면? => 그대로 temp로.
+        while (leftIndex <= center)
+        {
+            temp[tempIndex] = list[leftIndex];
+            leftIndex++;
+            tempIndex++;
+        }
+
+        // 오른쪽 범위에 남은 값이 있다면 => 그대로 temp로.
+        while (rightIndex <= right)
+        {
+            temp[tempIndex] = list[rightIndex];
+            rightIndex++;
+            tempIndex++;
+        }
+
+        // temp배열의 원소만 기존의 List에 덮어쓰기
+        for (int t = 0; t < temp.Length; t++)
+        {
+            list[left + t] = temp[t];
+        }
     }
 }
