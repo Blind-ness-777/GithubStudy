248ac0d3dd2bbd3dd90b3108f8442e845df3c06a
Blind-ness-777 <wlsgod10@gmail.com>
2026-01-09 10:07:44 +0900

    Updata

--- 260107/ConsoleApp1/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ 260107/ConsoleApp1/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15d875623e46ab3f7b3e097f403fa3b5dbb68592")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6afa782dc96e2678eaca9c8bb3323420260def6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
